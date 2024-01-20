32a0226d0003e5fc316b2d2e5e9d96397466b38d
Johnmarin93 <Johnmarin93@users.noreply.github.com>
2024-01-19 19:13:43 -0500

    modificacion origin

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app=express()
 // Cors  es un mecanismo basado en cabeceras HTTP que permite a un servidor indicar cualquier dominio
 app.use(cors(
     {
-        origin: 'http://localhost:5173',   // Tan solo por este 
+        origin: 'https://cred-dir.netlify.app',   // Tan solo por este 
+        /*http://localhost:5173*/ 
         credentials: true
     }
 ));
