d366d5fb68245fc3c5d649416a02d4f127004e1b
Johnmarin93 <Johnmarin93@users.noreply.github.com>
2024-01-22 22:58:53 -0500

    first commit

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ export const conectDB= async ()=>{
     
     //try catch para que si no hay problemas ejecute la conexion de lo contrario muestre el error por consola
     try {
-        await mongoose.connect('mongodb+srv://johnmarin:Sdh4nLOxBdQ9WrOk@cluster0.gtln7mq.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp');
+        await mongoose.connect(`${MONGODB_URI}`);
         console.log(">>> DB is connected")
     }catch (error){
         console.log(error);
